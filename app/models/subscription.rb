a33da77b375ee7892c7ec7279e49b08c9ccf2917
Brett Jones <bhjones4545@gmail.com>
2021-11-09 15:52:26 -0700

    Add subscription relationships and validations

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,12 @@
 class Subscription < ApplicationRecord
+  belongs_to :customer
+  belongs_to :tea
+
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :status, presence: true
+  validates :frequency, presence: true
+
+  enum status: {active: 0, inactive: 1}
+  enum frequency: {monthly: 0, yearly: 1}
 end
