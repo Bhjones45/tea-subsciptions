8b668a36e21c46056f6634582925b0f9421e993a
Brett Jones <bhjones4545@gmail.com>
2021-11-09 18:35:50 -0700

    Specify routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customers do
-        resources :subscriptions
+        resources :subscriptions, only: [:index, :create, :update]
       end
     end
   end
