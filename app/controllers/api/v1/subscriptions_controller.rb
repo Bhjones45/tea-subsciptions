628087d04e1145f16ccc198da167c293d4c4287c
Brett Jones <bhjones4545@gmail.com>
2021-11-12 09:50:27 -0700

    Create new strong param to only update status

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -3,13 +3,13 @@ class Api::V1::SubscriptionsController < ApplicationController
     customer = Customer.find(params[:customer_id])
     render json: SubscriptionSerializer.new(customer.subscriptions)
   end
-  
+
   def create
     customer = Customer.find(params[:customer_id])
     tea = Tea.find(params[:tea_id])
-    subscription = customer.subscriptions.create!(subscription_params.merge(tea_id: tea.id))
+    subscription = customer.subscriptions.create(subscription_params.merge(tea_id: tea.id))
     if subscription.save
-      render json: SubscriptionSerializer.new(subscription)
+      render json: SubscriptionSerializer.new(subscription) #201
     else
       render json: {:error => 'Record not found'}, status: 404
     end
@@ -17,12 +17,16 @@ class Api::V1::SubscriptionsController < ApplicationController
 
   def update
     subscription = Subscription.find(params[:id])
-    subscription.update(subscription_params)
+    subscription.update(update_sub_params)
     render json: SubscriptionSerializer.new(subscription)
   end
 
   private
   def subscription_params
     params.permit(:title, :price, :status, :frequency)
+  end
+
+  def update_sub_params
+    params.permit(:status)
   end
 end
